3245901eefec450cfd20603ea13f5735df5793f5
waynemamahit <wayne.mamahit@gmail.com>
2024-05-03 07:43:10 +0700

    config with pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bookstore-next',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      instances: 6,
+      exec_mode: 'cluster',
+    },
+  ],
+};
